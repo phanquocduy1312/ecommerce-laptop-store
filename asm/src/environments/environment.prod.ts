588360937f4c3d6318d81339b22b7c02734b867e
Phan Quoc Duy <phanquocduy1312y@gmail.com>
2026-04-30 00:14:59 +0700

    Configure for Vercel deployment

--- asm/src/environments/environment.prod.ts
+++ asm/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-production-api.com/api'  // URL production
+  apiUrl: 'https://laptop-store-backend.vercel.app/api'  // Update sau khi deploy backend
 };
